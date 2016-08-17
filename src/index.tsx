83f44618387eca733beb9f16dc6a0577d6886039
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2016-08-17 15:45:07 -0500

    An example with the react date range

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,43 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { DateRange } from "react-date-range";
+
+interface IMyComponentState {
+  startDate: Date;
+  endDate: Date;
+} 
 
 class MyComponent extends React.Component<{}, {}> {
+  state: IMyComponentState;
+  
+  constructor() {
+    super();
+
+    this.state = {
+      startDate: new Date(),
+      endDate: new Date(),
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(dates) {
+    console.log(dates);
+    
+    this.setState({
+      startDate: dates.startDate,
+      endDate: dates.endDate, 
+    });
+  }
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <DateRange
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
   }
 }
  
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,16 @@
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
+declare module "react-date-range" {
+    import * as React from 'react';
+
+    export interface IDateRangeProps {
+        onChange: Function;
+        endDate: Object;
+        startDate: Object;
+    }
+
+    export class DateRange extends React.Component<IDateRangeProps, {}> {
+    }
+    
+    export default DateRange;
+}
